8f54d53e0fea086591c7edb877680dba91d10bf2
Jason Crockett <JesusisLord33333@outlook.com>
2022-01-12 19:41:42 +0000

    complete :D

--- test/get_currencies_test.rb
+++ test/get_currencies_test.rb
@@ -13,4 +13,17 @@ class GetCurrenciesTest < Minitest::Test
   def test_contains_gbp_symbol
     assert_includes Tradeup::Database::Seeding.get_currencies, :GBP
   end
+
+  def test_does_not_contain_blacklist_items
+    symbols = Tradeup::Database::Seeding.get_currencies # list of currency symbols
+    Tradeup::Database::BLACKLIST.each do |item|
+      refute_includes symbols , item
+    end
+  end
+
+  def test_if_blacklist_does_not_contain_gbp
+    Tradeup::Database::BLACKLIST.each do |item|
+      refute_equal item, :GBP
+    end
+  end
 end
