7caed0498115041fa118129969f5b4ef618959d6
Jason Crockett <JesusisLord33333@outlook.com>
2022-01-13 16:58:52 +0000

    added test for test for get_rate

--- test/get_rate_test.rb
+++ test/get_rate_test.rb
@@ -10,8 +10,12 @@ class GetRateTest < Minitest::Test
     # Do nothing
   end
 
+  # incorrect environmental variables will be show in the debug log if any problems arise.
+
   def test_get_rate
     rate = Tradeup.get_rate 'GBP','USD'
+    puts 'Rate is'
+    puts rate.to_s
     assert_instance_of Float, rate
   end
 end
